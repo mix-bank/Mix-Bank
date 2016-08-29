83df45585ee1bfae228d7f2098c952746c038969
Ahmed Shaaban & Dylan <pair+ahmedShab.dylanhul@devbootcamp.com>
2016-08-30 11:56:25 +1200

    passes the test (connection, getting the right account id typ)

--- test/tests.js
+++ test/tests.js
@@ -2,49 +2,49 @@ var request = require ('supertest')
 var test = require ('tape')
 var server = require('../server.js')
 
-test('api conection', (t) => {
   request(server)
     .get('/api/v1/accounts/1/transactions')
-    .expect('Content-Type', "/json/")
     .expect(200)
     .end(function(err, res) {
-      t.error(err, "this is an error :(  :")
-      t.end()
+
+      test('api conection', (t) => {
+        t.error(err, "cannot connect to the API")
+        t.true(200)
+        t.end()
     })
 })
 
-test('typeof id', (t) => {
+  var expected = {
+    account:
+      {
+        id: 1,
+        transactions:[
+          {
+            id:503,
+            amount: 456,
+            description: 'purchase bk',
+            from_account_id: 1,
+            to_account_id: 2
+          },
+          {
+            id:504,
+            amount: 56700,
+            description: 'weekly pay from bk',
+            from_account_id: 2,
+            to_account_id: 1
+          }
+        ]
+      }
+
+  }
+
   request(server)
     .get('/api/v1/accounts/1/transactions')
-    .expect('Content-Type', "/json/")
     .expect(200)
     .end(function(err, res) {
-      t.error(err, "this is an error :(  :")
-      t.equal(res.data.account)
-      t.end()
+      test('typeof id', (t) => {
+        t.ifError(err, "this is an error")
+        t.true(typeof (res.body.account.id), 'number', "data.account.id should be a number")
+        t.end()
     })
 })
-
-var expected = {
-  account:
-    {
-      id: 1,
-      transactions:[
-        {
-          id:503,
-          amount: 456,
-          description: 'purchase bk',
-          from_account_id: 1,
-          to_account_id: 2
-        },
-        {
-          id:504,
-          amount: 56700,
-          description: 'weekly pay from bk',
-          from_account_id: 2,
-          to_account_id: 1
-        }
-      ]
-    }
-
-}
