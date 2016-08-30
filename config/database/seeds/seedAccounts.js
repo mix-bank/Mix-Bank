77e488ba0f837db54c84f3b3d93c19b56c08c003
Cameron Shotter <cameronshotter@gmail.com>
2016-08-31 11:03:22 +1200

    seed files changed to es6

--- config/database/seeds/seedAccounts.js
+++ config/database/seeds/seedAccounts.js
@@ -2,7 +2,7 @@
 exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
   return knex('accounts').del()
-    .then(function (){
+    .then(() => {
       return Promise.all([
         // Inserts seed entries
         knex('accounts').insert({id: 1, account_name: 'Ahmed'}),
