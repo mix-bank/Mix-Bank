f9989f1cf7ded0acd2d654368c4bb8d26c5d6384
James & Cameron Shotter <pair+JamesAnBo.cam-shotter@devbootcamp.com>
2016-08-30 13:54:11 +1200

    did the thing with the test stuff

--- test/tests.js
+++ test/tests.js
@@ -6,37 +6,13 @@ var server = require('../server.js')
     .get('/api/v1/accounts/1/transactions')
     .expect(200)
     .end(function(err, res) {
-
       test('api conection', (t) => {
-        t.error(err, "cannot connect to the API")
+        t.error(err, "No error")
         t.true(200)
         t.end()
     })
 })
 
-  var expected = {
-    account:
-      {
-        id: 1,
-        transactions:[
-          {
-            id:503,
-            amount: 456,
-            description: 'purchase bk',
-            from_account_id: 1,
-            to_account_id: 2
-          },
-          {
-            id:504,
-            amount: 56700,
-            description: 'weekly pay from bk',
-            from_account_id: 2,
-            to_account_id: 1
-          }
-        ]
-      }
-
-  }
 
   request(server)
     .get('/api/v1/accounts/1/transactions')
