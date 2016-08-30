bd4400400892464073371bc54a46a3b36827743f
Ahmed Shaaban & Dylan <pair+ahmedShab.dylanhul@devbootcamp.com>
2016-08-30 15:50:34 +1200

    updated package json for thunk

--- Client/index.js
+++ Client/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './components/app'
 import domready from 'domready'
 import { Provider } from 'react-redux'
-// import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
-// const store = createStore()
+let store = createStore(reducers, compose(
+    applyMiddleware(thunkMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ))
+
 
 
 
