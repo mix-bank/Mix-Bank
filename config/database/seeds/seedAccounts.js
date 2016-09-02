39903ce7de521b3b6fe7c28bcb90f5f261264bc2
Dylan & James <pair+DylanHul.JamesAnBo@devbootcamp.com>
2016-09-02 14:36:45 +1200

    hash is working. I added hashed password to the database as well

--- config/database/seeds/seedAccounts.js
+++ config/database/seeds/seedAccounts.js
@@ -5,9 +5,9 @@ exports.seed = (knex, Promise) => {
     .then(() => {
       return Promise.all([
         // Inserts seed entries
-        knex('accounts').insert({id: 1, account_name: 'Ahmed', account_email: 'ahmed@test.com', account_password: 'test1'}),
-        knex('accounts').insert({id: 2, account_name: 'James', account_email: 'james@test.com', account_password: 'test2'}),
-        knex('accounts').insert({id: 3, account_name: 'Dylan', account_email: 'dylan@test.com', account_password: 'test3'})
+        knex('accounts').insert({id: 1, account_name: 'Ahmed', account_email: 'ahmed@test.com', account_password: '$2a$10$skjmwSzquAIqBJrRB/B/Gea369DGsK5B1kOzBU0xtJpQOErWUO3oK'}),
+        knex('accounts').insert({id: 2, account_name: 'James', account_email: 'james@test.com', account_password: '$2a$10$2v9heMmv93ONRpguu.tsauvtKwalcMPZNFKFC6lUXds57rdpCfTN2'}),
+        knex('accounts').insert({id: 3, account_name: 'Dylan', account_email: 'dylan@test.com', account_password: '$2a$10$euxXvghd.ESb8WSWmQYQPenJByRPGysgKPKVr1YsnO5PklroxvSCa'})
       ]);
     });
 };
